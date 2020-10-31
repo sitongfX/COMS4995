93517555bd512b700b09b34783dc297a5ec2ba9f
Sitong Feng （冯思桐） [I] <sitfeng@tesla.com>
2020-10-31 20:06:45 +0800

    changed email

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,7 @@ class Mailer extends helper.Mail {
 
         this.sgApi = sendgrid(keys.sendGridKey);
 
-        this.from_email = new helper.Email('no-reply@servey-land.com');
+        this.from_email = new helper.Email('sitongf@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
